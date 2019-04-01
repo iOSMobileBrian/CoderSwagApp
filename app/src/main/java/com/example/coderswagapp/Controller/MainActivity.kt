4abf865c0d05d50ec08ba4deffbe5afd07d5a15c
iosmobilebrian <iosmobilebrian@gmail.com>
2019-03-31 20:22:55 -0400

    Data Model set-up

--- app/src/main/java/com/example/coderswagapp/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswagapp/Controller/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.coderswagapp
+package com.example.coderswagapp.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.coderswagapp.R
 
 class MainActivity : AppCompatActivity() {
 
---
+++ app/src/main/java/com/example/coderswagapp/Model/Product.kt
@@ -0,0 +1,5 @@
+package com.example.coderswagapp.Model
+
+import android.support.v7.widget.DialogTitle
+
+class Product(val title:String, val price: String, val image: String)
