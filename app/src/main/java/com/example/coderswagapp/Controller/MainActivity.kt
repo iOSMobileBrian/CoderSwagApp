6a759b052785767bc04760dca6cb464979cde57b
iosmobilebrian <iosmobilebrian@gmail.com>
2019-03-31 21:14:01 -0400

    Inital setup of ListView Layout

--- app/src/main/java/com/example/coderswagapp/Controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswagapp/Controller/MainActivity.kt
@@ -2,12 +2,23 @@ package com.example.coderswagapp.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.coderswagapp.Model.Category
 import com.example.coderswagapp.R
+import com.example.coderswagapp.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+            android.R.layout.simple_expandable_list_item_1, DataService.categories)
+
+        categoryListView.adapter = adapter
     }
 }
